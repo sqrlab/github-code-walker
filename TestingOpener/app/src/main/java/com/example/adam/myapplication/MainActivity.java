1e4ab3cc0584607d555d073c37d785dad4f82d48
Adam-Anthony <adam.a.pacheco@gmail.com>
2016-06-17 16:56:14 -0400

    Added syntax highlighting and comments

--- TestingOpener/app/src/main/java/com/example/adam/myapplication/MainActivity.java
+++ TestingOpener/app/src/main/java/com/example/adam/myapplication/MainActivity.java
@@ -11,7 +11,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.alorma.github.basesdk.client.BaseClient;
@@ -38,35 +42,45 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textView;
+    private Button button;
+    private WebView webView;
+    private ScrollView scrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Button?
+        button = (Button)findViewById(R.id.button);
+
+        //Scrollview?
+        scrollView = (ScrollView)findViewById(R.id.scrollView2);
+
+        //Opening Repo Files
         getReposFile getRepo = new getReposFile();
         getRepo.execute();
 
         //Textview Code area;
-        textView = (TextView) findViewById(R.id.text);
-        textView.setMovementMethod(new ScrollingMovementMethod());
+        webView = (WebView) findViewById(R.id.web);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
-        //Creating the Asynch task
-        getCode coder = new getCode();
-        //Calling asynch.
-        coder.execute("https://raw.githubusercontent.com/Adam-Anthony/JAX-/master/Java/BackEnd.java");
-
+        //HARDCODED STARTING PAGE
+        openIt("https://raw.githubusercontent.com/Adam-Anthony/JAX-/master/Java/BackEnd.java");
 
     }
 
     public void openIt(String a){
         //Creating the Asynch task
+        //String a: URL of download_file
+
         getCode coder = new getCode();
         //Calling asynch.
+
         coder.execute(a);
     }
 
+//HARD CODED DOWNLOAD FILE URL ACCESS
     public void openA(View view){
         openIt("https://raw.githubusercontent.com/Adam-Anthony/JAX-/master/Java/A%20-%20Run.bat");
     }
@@ -79,53 +93,77 @@ public class MainActivity extends AppCompatActivity {
     public void openD(View view){
         openIt("https://raw.githubusercontent.com/Adam-Anthony/JAX-/master/Java/Test.java");
     }
+//HARD CODED DOWNLOAD FILE URL ACCESS
 
+//
+    //What's this for?
+    public void hide(View view){
+        scrollView.setVisibility(view.GONE);
+    }
+
+    //
+    // Grabbing the repository information so we can actually access the folders and files
+    //
     protected class getReposFile extends AsyncTask<Void, Void, Void>{
         //private String[] alps;
         protected Void doInBackground (Void... params){
+            //Storing credentials
             StoreCredentials credentials = new StoreCredentials(getApplicationContext());
             credentials.storeToken(getString(R.string.token));
             SimpleDeveloperCredentialsProvider credentialsProvider = new SimpleDeveloperCredentialsProvider(null, null, null);
 
+            //Checking in credentials
             GithubDeveloperCredentials.getInstance();
             GithubDeveloperCredentials.init(credentialsProvider);
 
+            //Setting up the repo info
             final RepoInfo repoInfo = new RepoInfo();
+
+            //Repo owner name and Repository name
             repoInfo.owner = "Adam-Anthony";
             repoInfo.name = "JAX-";
 
+            //File info based on the repository information
             final FileInfo fileInfo = new FileInfo();
             fileInfo.repoInfo = repoInfo;
 
-
+            //Grabbing the Repo Content
             GetRepoContentsClient contentsClient = new GetRepoContentsClient(getApplicationContext(),repoInfo, "Java");
             contentsClient.setOnResultCallback(new BaseClient.OnResultCallback<List<Content>>() {
+                //OK Response
                 @Override
-                public void onResponseOk(final List<Content> contents, Response response) {
+                public void onResponseOk(final List<Content> contents, Response response) {    
+                    //List<Content>:    
+                    //Response:         
+
                     try {
                         final FileInfo fileInfo1 = new FileInfo();
                         fileInfo1.repoInfo = repoInfo;
                         fileInfo1.name = contents.get(0).name;
                         fileInfo1.content = contents.get(0).content;
                         fileInfo1.path = contents.get(0).path;
+                        fileInfo1.head = null;
+                        Log.d("FileInfo", fileInfo1.content);
                         for (int i = 0; i < 3; i++) {
                             LinearLayout linearLayout = (LinearLayout) findViewById(R.id.fileList);
                             TextView tv1 = new TextView(getApplicationContext());
                             tv1.setText(contents.get(i).name);
                             tv1.setPadding(5, 5, 5, 5);
+                            /*
                             tv1.setClickable(true);
                             tv1.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
                                     GetFileContentClient fileContentClient = new GetFileContentClient(getApplication(), fileInfo1);
-                                    fileContentClient.execute();
-                                    textView.setText("abcdefghijklmnopq");
+                                    Content c = fileContentClient.executeSync();
+                                    //textView.setText(c.name);
                                 }
                             });
                             linearLayout.addView(tv1);
+                            */
                         }
 
-                        String x = contents.get(0).name;
+                        //String x = contents.get(0).name;
                         /* Intent sending.
                         Intent intent = new Intent(getApplicationContext(), getReposFile.class);
                         intent.putExtra("val", x);
@@ -136,16 +174,23 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 }
+                //OK Response
 
+                //Fail Response
                 @Override
                 public void onFail(RetrofitError retrofitError) {
 
                 }
+                //Fail Response
             });
+            //Repo Content grabbed.
+            
             contentsClient.execute();
+            //Executing the contentsClient.
 
             return null;
         }
+
         protected void onPostExecute(){
 
             /* Intent recieving
@@ -156,11 +201,20 @@ public class MainActivity extends AppCompatActivity {
 
             TextView tv1 = new TextView(getApplication());
             tv1.setText("ABCDEFGHIJKLMNOP");
+            //Set a text view
+
             linearLayout.addView(tv1);
+            //Add to the linear layout
+
             FileInfo fileInfo = new FileInfo();
+            //Create a file info
         }
     }
 
+
+//
+// AsynchTask for reading the characters from the file  
+//
     protected class getCode extends AsyncTask<String, String, String>{
         @Override
         protected String doInBackground(String... params) {
@@ -169,14 +223,39 @@ public class MainActivity extends AppCompatActivity {
 
                 //Todo: Make the selectable files open their download urls and in this asynch task.
 
+                //Params[0] is the url of the download_file
                 URL url = new URL(params[0]);
+
+                //Split up the url based on .'s
+                String a = params[0];
+                String[] cut = a.split("\\.");
+                
+
+                //Grab the file type, used in setting the syntax highlighting, from the last cut[]
+                String type = "java";
+                type = cut[cut.length-1];
+                
                 BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
                 String code = "";
                 String str;
+                
+                //Opening Tags
+                code += "<!DOCTYPE html><html><head>" +
+                        "<link rel='stylesheet' type='text/css' href='highlight/styles/github.css'>" +
+                        "<script src='highlight/highlight.pack.js' type='text/javascript'></script>" +
+                        "<script src='highlightnumber/src/highlightjs-line-numbers.js' type='text/javascript'></script>" +
+                        "<script>hljs.initHighlightingOnLoad();</script>" + 
+                        "<script>hljs.initLineNumbersOnLoad();</script></head>"  +
+                        "<body><pre style='word-wrap: break-word; white-space: pre-wrap;'><code class='"+type+"'>";
+                
                 while ((str = in.readLine()) != null) {
+                    //Grabbing the characters from the file.
                     code = code + str + "\n";
                 }
                 in.close();
+                
+                //Closing Tags
+                code += "</code></pre></body></html>";
                 return code;
             } catch (MalformedURLException e) {
                 Log.d("x", "Malformed ");
@@ -186,12 +265,15 @@ public class MainActivity extends AppCompatActivity {
             return "";
         }
 
+        //Loading the webview with the file data.
         @Override
         protected void onPostExecute(String val) {
             super.onPostExecute(val);
-            textView.setText(val);
+            webView.loadDataWithBaseURL("file:///android_asset/",val, "text/html", "utf-8",null);
         }
     }
+
+    //Options Item Selected, generated on startup
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
